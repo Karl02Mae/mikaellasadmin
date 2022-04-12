2b823699a8a635393a0145fc880ccb1b4216e779
santoslirra <santoslirra@gmail.com>
2022-04-12 22:38:59 +0800

    test push

--- src/pages/AllBookings.js
+++ src/pages/AllBookings.js
@@ -2,8 +2,49 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ReportIcon from '@mui/icons-material/Report';
+import { DataGrid } from '@mui/x-data-grid';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import SettingsIcon from '@mui/icons-material/Settings';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
+const columns = [
+    { field: 'id', headerName: 'ID', width: 50 },
+    
+    {
+      field: 'customer',
+      headerName: 'Customer',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 240,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
 
+    { field: 'package', headerName: 'Package', width: 110 },
+    { field: 'booking', headerName: 'Booking', width: 110 },
+    { field: 'roomtype', headerName: 'Room Type', width: 110 },
+    { field: 'mobile', headerName: 'Mobile', width: 120 },
+    { field: 'checkin', headerName: 'Check In', width: 110 },
+    { field: 'checkout', headerName: 'Check Out', width: 110 },
+    { field: 'payment', headerName: 'Payment', width: 110 },
+    { field: 'addicon', headerName: <AddIcon />, width: 50 },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Walker', firstName: 'Sharon', package: 'Phase 1', booking: 'Active', roomtype: 'Family Room', mobile: '+124 394 1787', checkin: '10 Jan 2021', checkout: '15 Jan 2021', payment: 'Paid'  },
+    { id: 2, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 3, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 4, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 5, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 6, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 7, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 8, lastName: 'Walker', firstName: 'Sharon' },
+    { id: 9, lastName: 'Walker', firstName: 'Sharon' },
+  ];
+  
 const style = {
     AllBookingsContainer: {
         display: 'flex',
@@ -63,7 +104,60 @@ const style = {
         marginRight: 'auto',
         border: '1px solid black',
         borderRadius: '10px',
-    }
+    },
+    ButtonContainer: {
+        display: 'flex',
+        width: '97%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        border: '1px solid black',
+        borderRadius: '10px',
+      },
+      leftContainer: {
+        marginLeft: '-3px',
+        flexDirection: 'column',
+        border: '1px solid black',
+        borderRadius: '5px',
+        width: '1100px',
+        backgroundColor: '#f7f7f7',
+      },
+      ButtonRight: {
+        display: 'flex',
+    },
+    BulkActButton: {
+        backgroundColor: 'lightgray',
+        display: 'flex',
+        padding: '0.5px',
+        marginLeft: '10px',
+        alignItems: 'center',
+        border: '1.5px solid black',
+        borderRadius: '5px',
+        height: '30px',
+    },
+    BulkActText: {
+        fontSize: 'medium',
+        padding: 1,
+    },
+    ApplyButton: {
+        display: 'flex',
+        padding: '0.5px',
+        marginLeft: '10px',
+        alignItems: 'center',
+        border: '1.5px solid black',
+        borderRadius: '5px',
+        height: '30px',
+    },
+    ApplyText: {
+        fontSize: 'medium',
+        padding: 1,
+    },
+      rightContainer: {
+        flexDirection: 'column',
+        border: '1px solid black',
+        borderRadius: '5px',
+        width: '90px',
+        backgroundColor: '#f7f7f7',
+      },
 }
 
 export default function AllBookings() {
@@ -85,7 +179,54 @@ export default function AllBookings() {
                 </Box>
             </Box>
 
+            <Box sx={style.ButtonContainer} >
+
+                <Box sx={style.leftContainer}>
+
+                    <Box sx={style.ButtonRight}>
+                        <Box sx={style.BulkActButton}>
+                            <Typography sx={style.BulkActText}>Bulk Action</Typography>
+                            <ArrowDropDownIcon/>
+                        </Box>
+                        
+                        <Box sx={style.ApplyButton}>
+                            <Typography sx={style.ApplyText}>Apply</Typography>
+                        </Box>
+                    </Box>
+                
+                </Box>
+
+                <Box sx={style.rightContainer}>
+
+                    <Box sx={style.FilSetButton} >
+
+                        <Tooltip title="Filter list">
+                            <IconButton>
+                            <FilterListIcon />
+                            </IconButton>
+                        </Tooltip>
+
+                        <Tooltip title="Settings">
+                            <IconButton>
+                            <SettingsIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+
+                </Box>
+
+                
+            </Box>
+            
             <Box sx={style.AllBookingsListContainer}>
+ 
+                    <DataGrid
+                         rows={rows}
+                         columns={columns}
+                         pageSize={10}
+                         rowsPerPageOptions={[10]}
+                         checkboxSelection
+                     />
 
             </Box>
         </Box>
